ddfec9756d1e0bacc01e18b829cd5c5ad0724f15
joemphilips <joemphilips@gmail.com>
2018-02-27 12:41:30 +0900

    update

--- src/service.ts
+++ src/service.ts
@@ -6,7 +6,7 @@ import GRPCServer from "./rpc_server";
 import {DecryptStream, EncryptStream} from "./stream";
 
 
-// facade class of wallet.
+// facade class for wrapping up wallet with rpc interface
 // This is just one example of very basic wallet.
 // You may compose different kind of Classes and create different kinds of wallet.
 // e.g. wallet for managing community funds, wallet which uses external HD Key for signing, etc.
