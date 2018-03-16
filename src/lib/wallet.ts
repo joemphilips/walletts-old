afd0ea106b4d09ffb6fe3990902b56ff68002a11
joemphilips <joemphilips@gmail.com>
2018-03-16 23:30:25 +0900

    fix bug in blockchain proxy

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,7 +1,7 @@
 import WritableStream = NodeJS.WritableStream;
 import { Readable, Writable } from 'stream';
 import BackendProxy from './backend/node';
-import { BlockchainProxy, RPC } from './blockchain-proxy/trusted-rpc';
+import { BlockchainProxy, RPC } from './blockchain-proxy/';
 import CoinManager from './coin_manager';
 import {
   FailedToCreateWalletError,
