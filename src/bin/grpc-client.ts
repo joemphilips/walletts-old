eb1068be08a7be8c92bc9993379c6c7ee0fb09fd
joemphilips <joemphilips@gmail.com>
2018-03-19 14:42:53 +0900

    compile .proto file into same derectory
    * To avoid double compiling both with protoc and tsc

--- src/bin/grpc-client.ts
+++ src/bin/grpc-client.ts
@@ -1,7 +1,8 @@
 import * as grpc from 'grpc';
-
 import { PROTO_PATH } from './grpc-server';
+import logger from '../lib/logger';
 
+logger.error(`PROTO path is ${PROTO_PATH}`)
 // TODO: do not use any as a type
 const client: any = grpc.load(PROTO_PATH).lighthouse;
 export default client;
