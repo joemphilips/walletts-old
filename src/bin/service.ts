c7ce9a982b8d165ba161f86d68e98f63f091a079
joemphilips <joemphilips@gmail.com>
2018-03-20 13:36:31 +0900

    WIP: trying to make grpc work

--- src/bin/service.ts
+++ src/bin/service.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import {grpc} from 'grpc-web-client';
 import {
   Config,
   default as loadConfig,
@@ -7,9 +8,10 @@ import {
 import container from '../lib/container';
 import { BasicWallet } from '../lib/wallet';
 import WalletRepository from '../lib/wallet-repository';
-import client from './grpc-client';
 import GRPCServer from './grpc-server';
 import { UIProxy, WalletAction } from './uiproxy';
+import * as wallet_pb from 'proto/walletserver_pb'
+import {WalletService} from 'proto/walletserver_pb_service'
 
 export default class WalletLauncher {
   public readonly cfg: Config;
@@ -25,7 +27,6 @@ export default class WalletLauncher {
     this.server = container.resolve('server');
     this.uiproxy = container.resolve('uiproxy');
     this.logger = container.resolve('logger');
-    this.walletService = new client.WalletService(this.cfg.port);
   }
 
   public async run(): Promise<void> {
@@ -34,9 +35,21 @@ export default class WalletLauncher {
     chalk(`what do you want with your Wallet?`);
     const action: WalletAction = await this.uiproxy.setupWalletInteractive();
     if (action.kind === 'createWallet') {
-      this.walletService.CreateWallet({}, (res: any) => {
-        this.logger(`response for createWallet is ${res}`);
-      });
+      const req = new wallet_pb.createWalletRequest()
+      grpc.invoke(WalletService.createWallet, {
+        request: req,
+        host: this.cfg.port,
+        onMessage: (message: wallet_pb.createWalletResponse) => {
+          this.logger.info(`resopnse was ${message.toObject()}`)
+        },
+        onEnd: (code: grpc.Code, msg: string | undefined, trailer: grpc.Metadata) => {
+          if(code === grpc.Code.OK) {
+            this.logger.info(`received folloing message from server ${msg}`)
+          } else {
+            this.logger.error(`There was error with code ${code}, ${msg}`)
+          }
+        }
+      })
     } else if (action.kind === 'importWallet') {
       throw new Error('not supported yet!');
     } else if (action.kind === 'doNothing') {
