1d3f10c41c30ee42ab3ed91ab9219daa363a8228
joemphilips <joemphilips@gmail.com>
2018-04-19 12:10:50 +0900

    update Wallet Object to be more algebraic

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -4,31 +4,35 @@ import * as Logger from 'bunyan';
 import { crypto } from 'bitcoinjs-lib';
 import { Account, NormalAccount } from './account';
 /* tslint:disable-next-line  */
-import { Observable } from '@joemphilips/rxjs';
+import {Observable, Subject} from '@joemphilips/rxjs';
+import {DomainEvent} from "./primitives/event";
 
+type AccountMap = Map<AccountID, Account>
 export abstract class AbstractWallet {
   public abstract readonly id: AccountID;
-  public abstract readonly accounts: ReadonlyArray<Account> | null;
+  public abstract readonly accounts: AccountMap
 }
 
-interface WalletEvent {
-  kind: 'Created';
+interface WalletCreatedEvent extends DomainEvent {
+  type: 'walletCreated';
+  payload: { id: string }
 }
+
+type WalletEvent = WalletCreatedEvent
+type NormalAccountMap = Map<AccountID, NormalAccount>
 
 /**
  * Aggregate Root
  */
-export class BasicWallet extends Observable<WalletEvent>
+export class BasicWallet extends Subject<WalletEvent>
   implements AbstractWallet {
   private readonly logger: Logger | null;
   constructor(
     public readonly id: WalletID,
-    public readonly accounts: ReadonlyArray<Account> = [],
-    public parentLogger?: Logger
+    public readonly accounts: NormalAccountMap = new Map(),
+    readonly parentLogger: Logger
   ) {
     super();
-    this.logger = parentLogger
-      ? parentLogger.child({ subModule: 'BasicWallet' })
-      : null;
+    this.logger = parentLogger.child({ subModule: 'BasicWallet' })
   }
 }
