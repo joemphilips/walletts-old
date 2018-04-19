1ef36554c9aed7858da0d1731b1ce0180b68b25b
joemphilips <joemphilips@gmail.com>
2018-04-19 12:12:51 +0900

    fixup! update Wallet Object to be more algebraic

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -4,22 +4,22 @@ import * as Logger from 'bunyan';
 import { crypto } from 'bitcoinjs-lib';
 import { Account, NormalAccount } from './account';
 /* tslint:disable-next-line  */
-import {Observable, Subject} from '@joemphilips/rxjs';
-import {DomainEvent} from "./primitives/event";
+import { Observable, Subject } from '@joemphilips/rxjs';
+import { DomainEvent } from './primitives/event';
 
-type AccountMap = Map<AccountID, Account>
+type AccountMap = Map<AccountID, Account>;
 export abstract class AbstractWallet {
   public abstract readonly id: AccountID;
-  public abstract readonly accounts: AccountMap
+  public abstract readonly accounts: AccountMap;
 }
 
 interface WalletCreatedEvent extends DomainEvent {
   type: 'walletCreated';
-  payload: { id: string }
+  payload: { id: string };
 }
 
-type WalletEvent = WalletCreatedEvent
-type NormalAccountMap = Map<AccountID, NormalAccount>
+type WalletEvent = WalletCreatedEvent;
+type NormalAccountMap = Map<AccountID, NormalAccount>;
 
 /**
  * Aggregate Root
@@ -33,6 +33,6 @@ export class BasicWallet extends Subject<WalletEvent>
     readonly parentLogger: Logger
   ) {
     super();
-    this.logger = parentLogger.child({ subModule: 'BasicWallet' })
+    this.logger = parentLogger.child({ subModule: 'BasicWallet' });
   }
 }
